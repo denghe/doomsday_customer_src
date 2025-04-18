9c93202c447a8b9a080cb79ec84a47ae5cdb944a
denghe <denghe@hotmail.com>
2025-04-19 00:02:59 +0800

    add buff codes. enhance stat

---
+++ _src1/src/game_buff.h
@@ -0,0 +1,459 @@
+﻿#pragma once
+
+namespace Game {
+
+	enum class BuffTypes : int32_t {
+		__Default__ = 0,
+		_996,
+		中药调理,
+		跑步机,
+		眼镜,
+		肘击,
+		加班餐,
+		冲刺鞋子,
+		旋转办公椅,
+		塑料友情,
+		钢板,
+		海底捞,
+		义愤填膺,
+		国潮外卖,
+		躺平,
+		社交牛逼症,
+		风水大师,
+		CRUD糕手,
+		动漫达人,
+		屎山代码,
+		福报,
+		归宅部,
+		为什么我不是沪爷,
+		彩票研究者,
+		社恐,
+		肉食主义,
+		请假,
+		技术债,
+		南无加特林机枪菩萨,
+		南无阿弥陀佛,
+		沪爷,
+		坐在后面的好兄弟,
+		团队Leader,
+		sudo_rm_rf,
+		裁员大动脉,
+		筋肉兄贵,
+		投资,
+		劳动法,
+		__MaxValue__
+	};
+
+	static constexpr int32_t BuffLimits[(int32_t)BuffTypes::__MaxValue__] = {
+		0, // __Default__ = 0,
+		999, // _996,
+		999, // 中药调理,
+		999, // 跑步机,
+		999, // 眼镜,
+		999, // 肘击,
+		999, // 加班餐,
+		10, // 冲刺鞋子,
+		5, // 旋转办公椅,
+		999, // 塑料友情,
+		999, // 钢板,
+		999, // 海底捞,
+		999, // 义愤填膺,
+		1, // 国潮外卖,
+		1, // 躺平,
+		1, // 社交牛逼症,
+		999, // 风水大师,
+		999, // CRUD糕手,
+		999, // 动漫达人,
+		999, // 屎山代码,
+		1, // 福报,
+		3, // 归宅部,
+		999, // 为什么我不是沪爷,
+		1, // 彩票研究者,
+		1, // 社恐,
+		999, // 肉食主义,
+		1, // 请假,
+		999, // 技术债,
+		1, // 南无加特林机枪菩萨,
+		1, // 南无阿弥陀佛,
+		999, // 沪爷,
+		1, // 坐在后面的好兄弟,
+		1, // 团队Leader,
+		1, // sudo_rm_rf,
+		1, // 裁员大动脉,
+		999, // 筋肉兄贵,
+		1, // 投资,
+		1, // 劳动法,
+	};
+
+	static constexpr int32_t BuffRanks[(int32_t)BuffTypes::__MaxValue__] = {
+		0, // __Default__ = 0,
+		1, // _996,
+		1, // 中药调理,
+		1, // 跑步机,
+		1, // 眼镜,
+		1, // 肘击,
+		1, // 加班餐,
+		1, // 冲刺鞋子,
+		1, // 旋转办公椅,
+		1, // 塑料友情,
+		1, // 钢板,
+		1, // 海底捞,
+		1, // 义愤填膺,
+		1, // 国潮外卖,
+		2, // 躺平,
+		2, // 社交牛逼症,
+		2, // 风水大师,
+		2, // CRUD糕手,
+		2, // 动漫达人,
+		2, // 屎山代码,
+		2, // 福报,
+		2, // 归宅部,
+		2, // 为什么我不是沪爷,
+		2, // 彩票研究者,
+		2, // 社恐,
+		2, // 肉食主义,
+		2, // 请假,
+		2, // 技术债,
+		3, // 南无加特林机枪菩萨,
+		3, // 南无阿弥陀佛,
+		3, // 沪爷,
+		3, // 坐在后面的好兄弟,
+		3, // 团队Leader,
+		3, // sudo_rm_rf,
+		3, // 裁员大动脉,
+		3, // 筋肉兄贵,
+		3, // 投资,
+		3, // 劳动法,
+	};
+
+	template<BuffTypes bt> void BuffAddToSP(StatPanel& sp);
+	template<> void BuffAddToSP<BuffTypes::__Default__>(StatPanel& sp) {}
+
+	// 攻击+20%，攻速-5%，最大血量-5%
+	template<> void BuffAddToSP<BuffTypes::_996>(StatPanel& sp) {
+		sp.damageRatio += 0.2f;
+		sp.attackSpeed -= 0.05f;
+		sp.healthRatio -= 0.05f;
+	}
+
+	// 血量回复+10/s
+	template<> void BuffAddToSP<BuffTypes::中药调理>(StatPanel& sp) {
+		sp.healthRegeneration += 10;
+	}
+
+	// 最大血量+15%，下一回合以30%血量起步
+	template<> void BuffAddToSP<BuffTypes::跑步机>(StatPanel& sp) {
+		sp.healthRatio += 0.15f;
+		// todo: register round begin event?
+	}
+
+	// 范围+10，幸运-5
+	template<> void BuffAddToSP<BuffTypes::眼镜>(StatPanel& sp) {
+		sp.cameraZoom += 0.1f;
+		sp.luckyPoint -= 5;
+	}
+
+	// 射速+5%
+	template<> void BuffAddToSP<BuffTypes::肘击>(StatPanel& sp) {
+		sp.attackSpeed += 0.05f;
+	}
+
+	// 攻击+15%，血量回复-5/s
+	template<> void BuffAddToSP<BuffTypes::加班餐>(StatPanel& sp) {
+		sp.damageRatio += 0.15f;
+		sp.healthRegeneration -= 5;
+	}
+
+	// 移速+1
+	template<> void BuffAddToSP<BuffTypes::冲刺鞋子>(StatPanel& sp) {
+		sp.movementSpeedPoint += 1;
+	}
+
+	// 攻速+20%，移速-1
+	template<> void BuffAddToSP<BuffTypes::旋转办公椅>(StatPanel& sp) {
+		sp.attackSpeed += 0.2f;
+		sp.movementSpeedPoint -= 1;
+	}
+
+	// 攻速+10%，幸运-5，收获+5%
+	template<> void BuffAddToSP<BuffTypes::塑料友情>(StatPanel& sp) {
+		sp.attackSpeed += 0.1f;
+		sp.luckyPoint -= 5;
+		sp.harvestRatio += 0.05f;
+	}
+
+	// 护甲+5
+	template<> void BuffAddToSP<BuffTypes::钢板>(StatPanel& sp) {
+		sp.defensePoint += 5;
+	}
+
+	// 最大血量+5%，血量回复+10，伤害-5%
+	template<> void BuffAddToSP<BuffTypes::海底捞>(StatPanel& sp) {
+		sp.healthRatio += 0.05f;
+		sp.healthRegeneration += 10;
+		sp.damageRatio -= 0.05f;
+	}
+
+	// 暴击伤害+15%
+	template<> void BuffAddToSP<BuffTypes::义愤填膺>(StatPanel& sp) {
+		sp.criticalBonusRatio += 0.15f;
+	}
+
+	// 杀死小怪时有70%概率+30HP，30%概率-30HP
+	template<> void BuffAddToSP<BuffTypes::国潮外卖>(StatPanel& sp) {
+		// todo: register kill monster event?
+	}
+
+	// 攻击-20%，幸运+30，血量回复+15/s
+	template<> void BuffAddToSP<BuffTypes::躺平>(StatPanel& sp) {
+		sp.damageRatio -= 0.2f;
+		sp.luckyPoint += 30;
+		sp.healthRegeneration += 15;
+	}
+
+	// 攻击+30%，攻速+10%，每5秒不受控制的固定向前方冲刺0.5秒
+	template<> void BuffAddToSP<BuffTypes::社交牛逼症>(StatPanel& sp) {
+		sp.damageRatio += 0.3f;
+		sp.attackSpeed += 0.1f;
+		// todo: register player auto control skill?
+	}
+
+	// 幸运+40，攻击-5%
+	template<> void BuffAddToSP<BuffTypes::风水大师>(StatPanel& sp) {
+		sp.luckyPoint += 40;
+		sp.damageRatio -= 0.05f;
+	}
+
+	// 攻击-5%，攻速+30%
+	template<> void BuffAddToSP<BuffTypes::CRUD糕手>(StatPanel& sp) {
+		sp.damageRatio -= 0.05f;
+		sp.attackSpeed += 0.3f;
+	}
+
+	// 幸运+20，移速-1，血量回复+15/s
+	template<> void BuffAddToSP<BuffTypes::动漫达人>(StatPanel& sp) {
+		sp.luckyPoint += 20;
+		sp.movementSpeedPoint -= 1;
+		sp.healthRegeneration += 15;
+	}
+
+	// 攻击+15%，攻速+30%，血量回复-20/s
+	template<> void BuffAddToSP<BuffTypes::屎山代码>(StatPanel& sp) {
+		sp.damageRatio += 0.15f;
+		sp.attackSpeed += 0.3f;
+		sp.healthRegeneration -= 20;
+	}
+
+	// 最大血量-50%，攻速+100%，攻击-20%
+	template<> void BuffAddToSP<BuffTypes::福报>(StatPanel& sp) {
+		sp.healthRatio -= 0.5f;
+		sp.attackSpeed += 1;
+		sp.damageRatio -= 0.2f;
+	}
+
+	// 移速+2，闪避+3%
+	template<> void BuffAddToSP<BuffTypes::归宅部>(StatPanel& sp) {
+		sp.movementSpeedPoint += 2;
+		sp.dodgeRatio += 0.03f;
+	}
+
+	// 攻击+3%，幸运+5，血量回复+5/s，攻速+5%，最大血量+5%，暴击率+3%，爆伤+5%
+	template<> void BuffAddToSP<BuffTypes::为什么我不是沪爷>(StatPanel& sp) {
+		sp.damageRatio += 0.03f;
+		sp.luckyPoint += 5;
+		sp.healthRegeneration += 5;
+		sp.attackSpeed += 0.05f;
+		sp.healthRatio += 0.05f;
+		sp.criticalChance += 0.03f;
+		sp.criticalBonusRatio += 0.05f;
+	}
+
+	// 幸运 +60，攻击 -10%，血量回复 -5/s
+	template<> void BuffAddToSP<BuffTypes::彩票研究者>(StatPanel& sp) {
+		sp.luckyPoint += 60;
+		sp.damageRatio -= 0.1f;
+		sp.healthRegeneration -= 5;
+	}
+
+	// 闪避+15%，攻速 -10%，每3秒强制向后方冲刺0.5秒
+	template<> void BuffAddToSP<BuffTypes::社恐>(StatPanel& sp) {
+		sp.dodgeRatio += 0.15f;
+		sp.attackSpeed -= 0.1f;
+		// todo: register player auto control skill?
+	}
+
+	// 杀死小怪时攻击+3%（一局内）
+	template<> void BuffAddToSP<BuffTypes::肉食主义>(StatPanel& sp) {
+		// todo: register monster dead event? add extra store field?
+	}
+
+	// 按下特殊键F进入10秒无敌状态，但是不能行动。一局一次
+	template<> void BuffAddToSP<BuffTypes::请假>(StatPanel& sp) {
+		// todo: register player skill?
+	}
+
+	// 攻击+40%，攻速+20%，每回合最大HP-10%，收获-10%
+	template<> void BuffAddToSP<BuffTypes::技术债>(StatPanel& sp) {
+		sp.damageRatio += 0.4f;
+		sp.attackSpeed += 0.2f;
+		// todo: register round begin event? add extra store field?
+	}
+
+	// 攻击射出的子弹额外附加30%幸运值倍率的伤害
+	template<> void BuffAddToSP<BuffTypes::南无加特林机枪菩萨>(StatPanel& sp) {
+		// todo: add extra store field?
+	}
+
+	// 最大血量提升，提升值为300%幸运值
+	template<> void BuffAddToSP<BuffTypes::南无阿弥陀佛>(StatPanel& sp) {
+		// todo: add extra store field?
+	}
+
+	// 攻击-3%，幸运-5，血量回复-5/s，攻速-5%，最大血量-5%，暴击率-3%，爆伤-5%，收获+200
+	template<> void BuffAddToSP<BuffTypes::沪爷>(StatPanel& sp) {
+		sp.damageRatio -= 0.03f;
+		sp.luckyPoint -= 5;
+		sp.healthRegeneration -= 5;
+		sp.attackSpeed -= 0.05f;
+		sp.healthRatio -= 0.05f;
+		sp.criticalChance -= 0.03f;
+		sp.criticalBonusRatio -= 0.05f;
+		sp.harvestRatio += 2;
+	}
+
+	// 按F生成一个好兄弟，好兄弟会进入15秒无敌状态，和玩家拥有相同的攻击力，怪物会优先攻击好兄弟。15秒后结束。一局一次
+	template<> void BuffAddToSP<BuffTypes::坐在后面的好兄弟>(StatPanel& sp) {
+		// todo: register player skill?
+	}
+
+	// 生成3个好兄弟，拥有玩家50%攻击力和100%血量。自身攻击-25%，移速-1
+	template<> void BuffAddToSP<BuffTypes::团队Leader>(StatPanel& sp) {
+		// todo: register round begin event?
+		sp.damageRatio -= 0.25f;
+		sp.movementSpeedPoint -= 1;
+	}
+
+	// 移速+5，护甲+30，攻击-100%
+	template<> void BuffAddToSP<BuffTypes::sudo_rm_rf>(StatPanel& sp) {
+		sp.movementSpeedPoint += 5;
+		sp.defensePoint += 30;
+		sp.damageRatio -= 1;
+	}
+
+	// 死亡时回复20%HP保证不死，一局一次。触发时对全体怪物造成100%幸运值+300%倍率的伤害。
+	template<> void BuffAddToSP<BuffTypes::裁员大动脉>(StatPanel& sp) {
+		// todo: register player before dead event?
+	}
+
+	// 最大血量+50%，伤害+50%，下一回合以1%血量起步
+	template<> void BuffAddToSP<BuffTypes::筋肉兄贵>(StatPanel& sp) {
+		sp.healthRatio += 0.5f;
+		sp.damageRatio += 0.5f;
+		// todo: register round begin event?
+	}
+
+	// 每回合结束时-50%~+100%代币
+	template<> void BuffAddToSP<BuffTypes::投资>(StatPanel& sp) {
+		// todo: register round end ( before buy buff ) event?
+	}
+
+	// 无作用
+	template<> void BuffAddToSP<BuffTypes::劳动法>(StatPanel& sp) {}
+
+	typedef void (*Func_BuffAddToSP)(StatPanel&);
+	static constexpr Func_BuffAddToSP BuffAddToSPs[(int32_t)BuffTypes::__MaxValue__] = {
+		BuffAddToSP<BuffTypes::__Default__>,
+		BuffAddToSP<BuffTypes::_996>,
+		BuffAddToSP<BuffTypes::中药调理>,
+		BuffAddToSP<BuffTypes::跑步机>,
+		BuffAddToSP<BuffTypes::眼镜>,
+		BuffAddToSP<BuffTypes::肘击>,
+		BuffAddToSP<BuffTypes::加班餐>,
+		BuffAddToSP<BuffTypes::冲刺鞋子>,
+		BuffAddToSP<BuffTypes::旋转办公椅>,
+		BuffAddToSP<BuffTypes::塑料友情>,
+		BuffAddToSP<BuffTypes::钢板>,
+		BuffAddToSP<BuffTypes::海底捞>,
+		BuffAddToSP<BuffTypes::义愤填膺>,
+		BuffAddToSP<BuffTypes::国潮外卖>,
+		BuffAddToSP<BuffTypes::躺平>,
+		BuffAddToSP<BuffTypes::社交牛逼症>,
+		BuffAddToSP<BuffTypes::风水大师>,
+		BuffAddToSP<BuffTypes::CRUD糕手>,
+		BuffAddToSP<BuffTypes::动漫达人>,
+		BuffAddToSP<BuffTypes::屎山代码>,
+		BuffAddToSP<BuffTypes::福报>,
+		BuffAddToSP<BuffTypes::归宅部>,
+		BuffAddToSP<BuffTypes::为什么我不是沪爷>,
+		BuffAddToSP<BuffTypes::彩票研究者>,
+		BuffAddToSP<BuffTypes::社恐>,
+		BuffAddToSP<BuffTypes::肉食主义>,
+		BuffAddToSP<BuffTypes::请假>,
+		BuffAddToSP<BuffTypes::技术债>,
+		BuffAddToSP<BuffTypes::南无加特林机枪菩萨>,
+		BuffAddToSP<BuffTypes::南无阿弥陀佛>,
+		BuffAddToSP<BuffTypes::沪爷>,
+		BuffAddToSP<BuffTypes::坐在后面的好兄弟>,
+		BuffAddToSP<BuffTypes::团队Leader>,
+		BuffAddToSP<BuffTypes::sudo_rm_rf>,
+		BuffAddToSP<BuffTypes::裁员大动脉>,
+		BuffAddToSP<BuffTypes::筋肉兄贵>,
+		BuffAddToSP<BuffTypes::投资>,
+		BuffAddToSP<BuffTypes::劳动法>,
+	};
+
+	struct BuffContainer {
+		static constexpr int32_t numBuffers{ (int32_t)BuffTypes::__MaxValue__ };
+
+		std::array<int32_t, numBuffers> nums;
+		std::array<int8_t, numBuffers> flags;
+		xx::Listi32<std::pair<BuffTypes, int32_t>> logs;
+		StatPanel sp;
+
+		void Init() {
+			memset(&nums, 0, sizeof(nums));
+			memset(&flags, 0, sizeof(flags));
+			logs.Clear();
+			sp.Clear();
+		}
+
+		int32_t& NumOf(BuffTypes bt) const {
+			return (int32_t&)nums[(int32_t)bt];
+		}
+
+		int8_t& FlagOf(BuffTypes bt) const {
+			return (int8_t&)flags[(int32_t)bt];
+		}
+
+		std::pair<int32_t, int8_t> At(BuffTypes bt) const {
+			return { nums[(int32_t)bt], flags[(int32_t)bt] };
+		}
+
+		// for assert
+		bool IsLimited(BuffTypes bt) const {
+			assert(nums[(int32_t)bt] <= BuffLimits[(int32_t)bt]);
+			return nums[(int32_t)bt] == BuffLimits[(int32_t)bt];
+		}
+
+		bool TryAdd(BuffTypes bt) {
+			auto i = (int32_t)bt;
+			if (nums[i] < BuffLimits[i]) {
+				++nums[i];
+				BuffAddToSPs[i](sp);
+				logs.Emplace(bt, 1);
+				return true;
+			}
+			return false;
+		}
+
+		void NewRound() {
+			memset(&flags, 0, sizeof(flags));
+			// todo: cleanup some round sum values like "肉食主义" "技术债" ...
+		}
+
+		// todo: get drop list with exclude limited ?
+
+		// todo: get buff info for UI display
+	};
+
+}
--- _src1/src/game_stat.h
+++ _src1/src/game_stat.h
@@ -1,125 +1,69 @@
 ﻿#pragma once
-#include <looper.h>
-
-// unused
 
 namespace Game {
 
 	enum class StatTypes : uint32_t {
-		// base points
-		health,
-		vitality,
-		strength,
-		dodge,
-		agile,
-		dexterity,
-		defense,
-		wisdom,
-		lucky, __POINTS_END__ = lucky,
-
-		// results
-		life,					// nickname: health point / HP
-		lifeRegeneration,
-		energy,
-		energyRegeneration,
-		damageScale,
-		defenseScale,
-		evasion,
-		movementSpeed,
-		experienceScale,
+		healthPoint,
+		healthRatio,
+		healthRegeneration,
+		defensePoint,
+		dodgePoint,
+		dodgeRatio,
+		movementSpeedPoint,
+		damageRatio,
 		criticalChance,
-		criticalBonus, __RESULTS_END__ = criticalBonus,
-
-		// exts
-		//exts1,
-		//exts2,
-		//exts3,
-		// ...
-
+		criticalBonusRatio,
+		attackSpeed,
+		luckyPoint,
+		harvestRatio,
+		cameraScale,
 		__MAX_VALUE__
 	};
 
 	static constexpr std::array<const char*, (uint32_t)StatTypes::__MAX_VALUE__> strings_StatTypes{
-		"health",
-		"vitality",
-		"strength",
-		"dodge",
-		"agile",
-		"dexterity",
-		"defense",
-		"wisdom",
-		"lucky",
-
-		"life",
-		"lifeRegeneration",
-		"energy",
-		"energyRegeneration",
-		"damageScale",
-		"defenseScale",
-		"evasion",
-		"movementSpeed",
-		"experienceScale",
+		"healthPoint",
+		"healthRatio",
+		"healthRegeneration",
+		"defensePoint",
+		"dodgePoint",
+		"dodgeRatio",
+		"movementSpeedPoint",
+		"damageRatio",
 		"criticalChance",
-		"criticalBonus",
-
-		//"exts1",
-		//"exts2",
-		//"exts3",
-		// ...
+		"criticalBonusRatio",
+		"attackSpeed",
+		"luckyPoint",
+		"harvestRatio",
+		"cameraZoom",
 	};
 
 	using Stat_t = float;
-	//using Stat_t = double;
-
 	struct alignas(8) StatItem {
-		StatTypes type{};
-		Stat_t value{};
+		StatTypes type;
+		Stat_t value;
 	};
 
-	struct alignas(8) StatPanelBase {
-		static constexpr int32_t numFields{ (int32_t)StatTypes::__POINTS_END__ + 1 };
-		Stat_t health{};						// + life max, energy max
-		Stat_t vitality{};						// + life regeneration, energy regeneration
-		Stat_t strength{};						// + damage scale
-		Stat_t dodge{};							// + evasion
-		Stat_t agile{};							// + movement speed
-		Stat_t dexterity{};						// + attack speed, critical bonus
-		Stat_t defense{};						// + damage reduce scale
-		Stat_t wisdom{};						// + experience scale
-		Stat_t lucky{};							// + critical chance, improve drop rate
-		constexpr Stat_t& operator[](int32_t idx) const {
-			assert(idx <= SP::numFields);
-			return ((Stat_t*)this)[idx];
-		}
-	};
-
-	struct alignas(8) StatPanelResult {
-		static constexpr int32_t numFields{ (int32_t)StatTypes::__RESULTS_END__ - (int32_t)StatTypes::__POINTS_END__ };
-		Stat_t life{};							// from health			// max val
-		Stat_t lifeRegeneration{};				// from vitality		// restore runtime life per seconds
-		Stat_t energy{};						// from health			// max val
-		Stat_t energyRegeneration{};			// from vitality		// restore runtime energy per seconds
-		Stat_t damageScale{};					// from strength		// final damage = weapon damage * damageScale
-		Stat_t evasion{};						// from dodge			// chance 0 ~ 1
-		Stat_t movementSpeed{};					// from agile			// position change max pixel per seconds
-		Stat_t attackSpeed{};					// from dexterity		// attack times per seconds
-		Stat_t defenseScale{};					// from defense			// final damage = damage * defenseScale
-		Stat_t experienceScale{};				// from wisdom			// final exp = exp * experienceScale
-		Stat_t criticalChance{};				// from lucky			// if rand(0, 1) < criticalChance then final damage = damage * (1 + criticalBonus)
-		Stat_t criticalBonus{};					// from dexterity		// for crtical hit damage calculate ( scale )
-		void ZeroResult() {
-			memset(this, 0, sizeof(StatPanelResult));
+	struct alignas(8) StatPanel {
+		static constexpr int32_t numFields{ (int32_t)StatTypes::__MAX_VALUE__ };
+		Stat_t healthPoint;
+		Stat_t healthRatio;
+		Stat_t healthRegeneration;
+		Stat_t defensePoint;
+		Stat_t dodgePoint;
+		Stat_t dodgeRatio;
+		Stat_t movementSpeedPoint;
+		Stat_t damageRatio;
+		Stat_t criticalChance;
+		Stat_t criticalBonusRatio;
+		Stat_t attackSpeed;		// times per seconds
+		Stat_t luckyPoint;
+		Stat_t harvestRatio;
+		Stat_t cameraZoom;
+		void Clear() {
+			memset(this, 0, sizeof(StatPanel));
 		}
 		constexpr Stat_t& operator[](int32_t idx) const {
-			assert(idx <= SP::numFields);
-			return ((Stat_t*)this)[idx];
-		}
-	};
-
-	struct alignas(8) StatPanel : StatPanelBase, StatPanelResult {
-		static constexpr int32_t numFields{ (int32_t)StatTypes::__RESULTS_END__ + 1 };
-		constexpr Stat_t& operator[](int32_t idx) const {
-			assert(idx <= SP::numFields);
+			assert(idx <= numFields);
 			return ((Stat_t*)this)[idx];
 		}
 		void Dump() {
@@ -130,322 +74,61 @@ namespace Game {
 	};
 
 	struct StatCfg {
-		// rules
-		static constexpr bool enableCritChanceToBonus{ false };
-		static constexpr Stat_t evasionFactor{ 16 };
+		static constexpr Stat_t baseMovementSpeed{ 300 / 5 };
+		static constexpr Stat_t dodgeFactor{ 16 };
 		static constexpr Stat_t defenseFactor{ 66 };
-		static constexpr StatPanelResult rangeFrom{};	// all zero
-		static constexpr StatPanelResult rangeTo{
-			.life = 1000000,
-			.lifeRegeneration = 1000000,
-			.energy = 1000000,
-			.energyRegeneration = 1000000,
-			.damageScale = 1000,
-			.evasion = 0.8,
-			.movementSpeed = 2000,
-			.attackSpeed = 100,
-			.defenseScale = 0.99999,
-			.experienceScale = 1000,
-			.criticalChance = 1000,
-			.criticalBonus = 1000,
+		static constexpr Stat_t luckyToHealthRegenerationRatio{ 0.01 };
+		static constexpr Stat_t luckyToDamageRatio{ 0.01 };
+		static constexpr Stat_t luckyTocriticalChanceRatio{ 0.001 };
+
+		static constexpr StatPanel rangeFrom{	// min val
+			.healthPoint = 1,
+			.healthRatio = 1,
+			.healthRegeneration = 0,
+			.defensePoint = 0,
+			.dodgePoint = 0,
+			.dodgeRatio = 0,
+			.movementSpeedPoint = 0,
+			.damageRatio = 0,
+			.criticalChance = 0,
+			.criticalBonusRatio = 0,
+			.attackSpeed = 0,
+			.luckyPoint = 0,
+			.harvestRatio = 0,
+			.cameraZoom = 1,
 		};
-		StatPanelBase init;						// first time fill to curr value
-		StatPanelBase levelRatio;				// every time when level up: curr value *= ratio
-		StatPanelResult resultRatio;			// result = curr value * ratio
-
-		template<bool fillPoint = true, bool fillLevelRatio = true, bool fillResultRatio = true>
-		void Init(Stat_t defaultPoint = 0, Stat_t defaultLevelRatio = 1.1, Stat_t defaultResultRatio = 1) {
-			if constexpr (fillPoint) {
-				for (int32_t i = 0; i < init.numFields; ++i) {
-					init[i] = defaultPoint;
-				}
-			}
-			if constexpr (fillLevelRatio) {
-				for (int32_t i = 0; i < levelRatio.numFields; ++i) {
-					levelRatio[i] = defaultLevelRatio;
-				}
-			}
-			if constexpr (fillResultRatio) {
-				for (int32_t i = 0; i < resultRatio.numFields; ++i) {
-					resultRatio[i] = defaultResultRatio;
-				}
-			}
-		}
-
-		template<typename C>
-		void Calc(StatPanelBase& spb, StatPanel& sp, int32_t& currLevel, int32_t tarLevel, C const& equipments) const {
-			// upgrade base points by target level
-			if (currLevel == 0) {
-				memcpy(&spb, &init, sizeof(spb));
-				currLevel = 1;
-			}
-			while (currLevel < tarLevel) {
-				for (int32_t i = 0; i < spb.numFields; ++i) {
-					spb[i] *= levelRatio[i];
-					sp[i] = spb[i];
-				}
-				++currLevel;
-			}
-			// gather points & results from items
-			sp.ZeroResult();
-			if (auto equipmentsCount = equipments.Count(); equipmentsCount > 0) {
-				for (int32_t ei = 0; ei < equipmentsCount; ++ei) {
-					auto& e = equipments[ei];
-					if (auto statsCount = e->stats.Count(); statsCount > 0) {
-						for (int32_t si = 0; si < statsCount; ++si) {
-							auto& s = e->stats[si];
-							sp[(uint32_t)s.type] += s.value;
-						}
-					}
-				}
-			}
-			// calculate final results
-			sp.life += sp.health * resultRatio.life;
-			sp.energy += sp.health * resultRatio.energy;
-			sp.lifeRegeneration += sp.vitality * resultRatio.lifeRegeneration;
-			sp.energyRegeneration += sp.vitality * resultRatio.energyRegeneration;
-			sp.damageScale += sp.strength * resultRatio.damageScale;
-			sp.evasion += sp.dodge / (sp.dodge + this->evasionFactor);
-			sp.movementSpeed += sp.agile * resultRatio.movementSpeed;
-			sp.attackSpeed += sp.dexterity * resultRatio.attackSpeed;
-			sp.defenseScale += sp.defense / (sp.defense + this->defenseFactor);
-			sp.experienceScale += sp.wisdom * resultRatio.experienceScale;
-			sp.criticalChance += sp.lucky * resultRatio.criticalChance;
-			sp.criticalBonus += sp.dexterity * resultRatio.criticalBonus;
-			// apply rules
-			auto& spr = *(StatPanelResult*)&sp;
-			for (int32_t i = 0; i < StatPanelResult::numFields; ++i) {
-				auto& v = spr[i];
-				if (v < rangeFrom[i]) v = rangeFrom[i];
-				else if (v > rangeTo[i]) v = rangeTo[i];
-			}
-			if (sp.criticalChance > 1 && enableCritChanceToBonus) sp.criticalBonus += sp.criticalChance - 1;
-		}
+		static constexpr StatPanel rangeTo{		// max val
+			.healthPoint = 999999,
+			.healthRatio = 999999,
+			.healthRegeneration = 999999,
+			.defensePoint = 999999,
+			.dodgePoint = 65,
+			.dodgeRatio = 1,
+			.movementSpeedPoint = 25,
+			.damageRatio = 999999,
+			.criticalChance = 1,
+			.criticalBonusRatio = 1.5,
+			.attackSpeed = 999999,
+			.luckyPoint = 1000,
+			.harvestRatio = 999999,
+			.cameraZoom = 10,
+		};
+		StatPanel init{							// init val
+			.healthPoint = 1000,
+			.healthRatio = 1,
+			.healthRegeneration = 5,
+			.defensePoint = 0,
+			.dodgePoint = 0,
+			.dodgeRatio = 0,
+			.movementSpeedPoint = 5,
+			.damageRatio = 1,
+			.criticalChance = 0.2,
+			.criticalBonusRatio = 1.5,
+			.attackSpeed = 1,
+			.luckyPoint = 10,
+			.harvestRatio = 0,
+			.cameraZoom = 1,
+		};
 	};
 
-	template<typename E>
-	struct StatExt {
-		xx::TinyList<xx::Shared<E>> equipments;
-		int32_t level{}, experience{};
-		Stat_t life{}, energy{};
-		bool spDirty{ true };
-		StatPanelBase spb;		// backup base point for level up
-		StatPanel sp;
-		StatCfg statCfg;
-
-		void InitStat(int32_t tarLevel) {
-			assert(level == 0);
-			assert(experience == 0);
-			statCfg.Calc(spb, sp, level, tarLevel, equipments);
-			life = sp.life;
-			energy = sp.energy;
-		}
-	};
-
-};
-
-/*********************************************************************************************/
-/*********************************************************************************************/
-// ref codes
-
-
-//struct Creature;
-//struct Equipment {
-//	virtual ~Equipment() {}
-//	xx::Weak<Creature> owner;
-//	xx::TinyList<StatItem> stats;		// maybe use fixed length array instead of list
-//	// ...
-//};
-
-
-
-//struct alignas(8) StatPanelExt : StatPanel {
-//	Stat_t exts1;
-//	Stat_t exts2;
-//	Stat_t exts3;
-//	// ...
-//	//Stat_t dummy[1000];
-
-//	void Clear() {
-//		memset(this, 0, sizeof(StatPanelExt));
-//	}
-
-//	XX_INLINE Stat_t& At(StatTypes t) const {
-//		assert((uint32_t)t <= (uint32_t)StatTypes::__MAX_VALUE__);
-//		return ((Stat_t*)this)[(uint32_t)t];
-//	}
-
-//	void Dump() {
-//		for (uint32_t i = 0; i <= (uint32_t)StatTypes::__MAX_VALUE__; ++i) {
-//			xx::CoutN(strings_StatTypes[i], " = ", At((StatTypes)i));
-//		}
-//	}
-//};
-
-
-
-//struct Creature {
-//	// ...
-
-//	xx::Ref<StatCfg> statCfg;
-//	xx::TinyList<xx::Shared<Equipment>> equipments;
-//	Stat_t level{}, experience{};
-//	Stat_t life{}, energy{};
-
-//	bool spDirty{ true };
-//	StatPanel sp;
-
-//	void Init() {
-//		level = 1;
-//		experience = 0;
-//		statCfg->Calc(sp, level, equipments);
-//		life = sp.life;
-//		energy = sp.energy;
-//	}
-
-//	void UpdateSP() {
-//		if (spDirty) {
-//			spDirty = false;
-//			statCfg->Calc(sp, level, equipments);
-//		}
-//		// handle regenerations
-//		if (life < sp.life) {
-//			life += sp.lifeRegeneration;
-//			if (life > sp.life) life = sp.life;
-//		}
-//		if (energy < sp.energy) {
-//			energy += sp.energyRegeneration;
-//			if (energy > sp.energy) energy = sp.energy;
-//		}
-//	}
-
-//	void Update() {
-//		UpdateSP();	// need update all monster sp first
-//		// ...
-//	}
-//};
-
-
-
-
-//struct Equipment1 : Equipment {
-//	Stat_t damage{};
-//	Stat_t attackSpeed{};
-//	Equipment1(xx::Shared<Creature> const& owner_) {
-//		owner = owner_;
-//		damage = 1;
-//		attackSpeed = 2;
-//		stats.Emplace(StatTypes::health, 5);
-//		stats.Emplace(StatTypes::vitality, 6);
-//	}
-//};
-
-
-
-//struct Equipment2 : Equipment {
-//	Equipment2(xx::Shared<Creature> const& owner_) {
-//		owner = owner_;
-//		stats.Emplace(StatTypes::movementSpeed, 100);
-//	}
-//};
-
-
-
-//struct Projectile {
-//	xx::Weak<Creature> owner;
-//	xx::Weak<Equipment> emitter;
-//	virtual ~Projectile() {};
-//	virtual bool Hurt(Creature* tar) { return false; }
-//};
-
-
-
-//struct Bullet1 : Projectile {
-//	Stat_t damage{};											// emitter's props copy here
-//	Stat_t damageScale{}, criticalChance{}, criticalBonus{};	// owner's props copy here
-
-//	void Init(Equipment1* emitter_) {
-//		assert(emitter_);
-//		owner = emitter_->owner;
-//		emitter = xx::WeakFromThis(emitter_);
-//		auto& sp = emitter_->owner->sp;
-//		// copy props
-//		damage = emitter_->damage;
-//		damageScale = sp.damageScale;
-//		criticalChance = sp.criticalChance;
-//		criticalBonus = sp.criticalBonus;
-//	}
-
-//	// tar->life -= d ( tar->life == 0  mean  is dead )
-//	bool Hurt(Creature* tar) override {
-//		auto d = damage * damageScale;
-//		auto crit = gLooper.rnd.Next<float>() < criticalChance;
-//		if (crit) {
-//			d += d * criticalBonus;
-//		}
-//		tar->life -= d;
-//		if (tar->life <= 0) {
-//			tar->life = 0;
-//			// todo: emitter + exp ? update: check exp & level up ?
-//		}
-//		return crit;
-//	}
-//};
-
-
-
-//inline void Test() {
-
-//	xx::Listi32<xx::Shared<Creature>> chars;
-//	for (int j = 0; j < 100000; j++) {
-//		auto c = chars.Emplace().Emplace();
-//		c->cfg.Emplace();
-//		c->equipments.Emplace( xx::MakeShared<Equipment1>(c) );
-//		c->equipments.Emplace( xx::MakeShared<Equipment1>(c) );
-//		c->equipments.Emplace( xx::MakeShared<Equipment1>(c) );
-//		c->equipments.Emplace( xx::MakeShared<Equipment2>(c) );
-//		c->equipments.Emplace( xx::MakeShared<Equipment2>(c) );
-//		c->equipments.Emplace( xx::MakeShared<Equipment2>(c) );
-//		c->Init();
-//	}
-//	auto secs = xx::NowEpochSeconds();
-//	for (int i = 0; i < 1000; ++i) {
-//		for (int j = 0; j < 100000; j++) {
-//			chars[j]->Update();
-//		}
-//	}
-//	xx::CoutN("secs = ", xx::NowEpochSeconds(secs));
-
-//	{
-//		auto c = chars[0];
-//		xx::CoutN("after Update()");
-//		c->sp.Dump();
-//		xx::CoutN("simulate hurt");
-//		c->life -= 10;
-//		xx::CoutN("life = ", c->life);
-//		c->Update();
-//		xx::CoutN("life = ", c->life);
-//		c->Update();
-//		xx::CoutN("life = ", c->life);
-//		c->level++;
-//		c->Update();
-//		xx::CoutN("level = ", c->level);
-//		xx::CoutN("life = ", c->life);
-//		c->sp.Dump();
-//		xx::CoutN("bullet hurt test");
-//		auto b = xx::MakeShared<Bullet1>();
-//		b->Init(dynamic_cast<Equipment1*>(c->equipments[0].pointer));
-//		auto r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//		r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//		r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//		r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//		r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//		r = b->Hurt(c);
-//		xx::CoutN("r = ", r, " life = ", c->life);
-//	}
-//}
+}
