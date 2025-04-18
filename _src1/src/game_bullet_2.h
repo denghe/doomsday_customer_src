07d7dfd80b17bc93ae5ad7266ea6821f72aa267b
belmont <672317451@qq.com>
2025-04-19 00:29:05 +0800

    save code

--- _src1/src/game_bullet_2.h
+++ _src1/src/game_bullet_2.h
@@ -6,7 +6,7 @@ namespace Game {
 		XY inc{};							// move increase pixel per frame
 		int32_t lifeEndTime{};				// max life point
 
-		void Init(Skill_Shoot_1* skill, XY const& pos_, float radians_, float lifeSeconds_, float cos, float sin);
+		void Init(Skill_Shoot_2* skill, XY const& pos_, float radians_, float lifeSeconds_, float cos, float sin);
 		int32_t Update() override;
 		void Draw() override;
 	};
--- _src1/src/game_bullet_2.hpp
+++ _src1/src/game_bullet_2.hpp
@@ -2,7 +2,7 @@
 
 namespace Game {
 
-	void Bullet_2::Init(Skill_Shoot_1* skill, XY const& pos_, float radians_, float lifeSeconds_, float cos, float sin) {
+	void Bullet_2::Init(Skill_Shoot_2* skill, XY const& pos_, float radians_, float lifeSeconds_, float cos, float sin) {
 		stage = skill->creature->stage;
 		frame = gLooper.res.bullet_coin5;
 
@@ -30,6 +30,7 @@ namespace Game {
 		auto dd = d.x * d.x + d.y * d.y;
 		auto r2 = player->radius + radius;
 		if (dd < r2 * r2) {
+			xx::CoutN(U"Hit Player");
 			return 1;
 		}
         return lifeEndTime < stage->time;
