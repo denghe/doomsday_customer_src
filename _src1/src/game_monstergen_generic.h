55c698f42e5cbd1215728a25aab46ec04bf5db17
denghe <denghe@hotmail.com>
2025-04-22 15:01:24 +0800

    add  MonsterGen_2xHamburger ( not test )

--- _src1/src/game_monstergen_generic.h
+++ _src1/src/game_monstergen_generic.h
@@ -7,4 +7,22 @@ namespace Game {
     struct MonsterGen_Generic : MonsterGen {
         int32_t Update() override;
     };
+
+
+    struct MonsterGen_2xHamburger : MonsterGen {
+        static constexpr float cRadius{ 2 };
+        static constexpr float cDurationSeconds{ 1 };
+        static constexpr int32_t cDurationNumFrames{ int32_t(Cfg::fps * cDurationSeconds) };
+        static constexpr int32_t cNumMonsters{ 2 };
+        static constexpr int32_t cStepNumFrames{ cDurationNumFrames / cNumMonsters };
+
+        int32_t numGroups{};
+        int32_t groupDelayNumFrames{};
+        int32_t n{}, i{}, j{}, t{};
+        XY pos{};
+
+        void Init(Stage* stage_, float activeSeconds_, float destroySeconds_, int32_t numGroups_);
+        int32_t Update() override;
+    };
+
 }
--- _src1/src/game_monstergen_generic.hpp
+++ _src1/src/game_monstergen_generic.hpp
@@ -8,4 +8,49 @@ namespace Game {
 		CreateFromFullMap<T>();
 		return 0;
 	}
+
+
+	inline void MonsterGen_2xHamburger::Init(Stage* stage_, float activeSeconds_, float destroySeconds_, int32_t numGroups_) {
+		stage = stage_;
+		activeTime = stage_->time + int32_t(Cfg::fps * activeSeconds_);
+		destroyTime = stage_->time + int32_t(Cfg::fps * destroySeconds_);
+		numGroups = numGroups_;
+
+		auto d = destroyTime - activeTime;
+		assert(d >= cStepNumFrames * numGroups_);
+		groupDelayNumFrames = d / numGroups_ - cStepNumFrames;
+	}
+
+	inline int32_t MonsterGen_2xHamburger::Update() {
+		XX_BEGIN(n);
+
+		for (j = 0; j < numGroups; ++j) {
+
+			pos.x = stage->rnd.Next<float>(cRadius, stage->mapSize.x - cRadius);
+			pos.y = stage->rnd.Next<float>(cRadius, stage->mapSize.y - cRadius);
+			for (i = 0; i < cNumMonsters; ++i) {
+				{
+					auto x = pos.x + stage->rnd.Next<float>(-cRadius, cRadius);
+					auto y = pos.y + stage->rnd.Next<float>(-cRadius, cRadius);
+					stage->spawners.Emplace().Emplace()->Init(stage, statCfg, { x,y }, 1.f, 1.5f, [](Stage* stage_, StatCfg const& statCfg_, XY const& pos_) {
+						auto m = xx::MakeShared<Monster_Hamburger>();
+						m->Init(stage_, statCfg_, pos_);
+						stage_->monsters.Add(std::move(m));
+						});
+				}
+
+				for (t = stage->time + cStepNumFrames; stage->time < t;) {
+					XX_YIELD_I(n);
+				}
+			}
+
+			for (t = stage->time + groupDelayNumFrames; stage->time < t;) {
+				XX_YIELD_I(n);
+			}
+		}
+
+		return 1;
+
+		XX_END(n);
+	}
 }
