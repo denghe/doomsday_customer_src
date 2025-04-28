4c6b3d9e26ed2b2a74e8cde7668d6d61e59fca08
belmont <672317451@qq.com>
2025-04-28 11:13:08 +0800

    round config

--- _src1/src/game_stage1.hpp
+++ _src1/src/game_stage1.hpp
@@ -6,18 +6,18 @@ namespace Game {
 		ui.Emplace()->Init();
 		ui->MakeChildren<xx::Button>()->Init(1, Cfg::xy1m, Cfg::xy1a, gLooper.btnCfg, U"exit", [&]() {
 			gLooper.DelaySwitchTo<Game::MainMenu>();
-		});
+			});
 
 		static constexpr float imgBtnSize{ 64.f / 1080.f * Cfg::height };
 		uiHUD.Emplace()->Init();
 		// make pause button
 		uiHUD->MakeChildren<xx::ImageButton>()->Init(1, Cfg::xy9m, Cfg::xy9a, imgBtnSize, gLooper.res.ui_pause).onClicked = [&]() {
 			uiPausePanel.Popup();
-		};
+			};
 		// make shop button
 		uiHUD->MakeChildren<xx::ImageButton>()->Init(1, Cfg::xy8m, Cfg::xy8a, imgBtnSize, gLooper.res.ui_money).onClicked = [&]() {
 			uiShopPanel.Popup();
-		};
+			};
 
 		uiHPBar.Init(this);
 		uiCoinBar.Init(this);
@@ -25,7 +25,7 @@ namespace Game {
 		uiShopPanel.Init(this);
 		// ...
 
-		gridSize = {30, 20};
+		gridSize = { 30, 20 };
 		mapSize = Cfg::unitSize * gridSize;
 
 		camera.scale = Cfg::defaultScale;
@@ -55,190 +55,202 @@ namespace Game {
 		// call round begin event handler
 		player->OnRoundBegin();
 
+		auto gen2Hamburger = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
+				, activeSeconds_, destroySeconds, 2, Cfg::unitRadius * 2, 1, numGroups);
+			};
+		auto gen3Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 3, Cfg::unitRadius * 2, 1, numGroups);
+			};
+		auto gen4Hamburger1Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 4, 1, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen1RoastDuck1Cola4Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group3<Monster_Roastduck, Monster_Cola, Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 1, 1, 4, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen3Cola = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
+				, activeSeconds_, destroySeconds, 3, Cfg::unitRadius * 2, 1.5, numGroups);
+			};
+		auto gen15Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 15, Cfg::unitRadius * 4, 4, numGroups);
+			};
+		auto gen1RoastDuck3Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 1,3, Cfg::unitRadius * 2, 1.5, numGroups);
+			};
+		auto gen3RoastDuck = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Roastduck>>()->Init(this
+				, activeSeconds_, destroySeconds, 3, Cfg::unitRadius * 2, 2, numGroups);
+			};
+		auto gen5Hamburger = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
+				, activeSeconds_, destroySeconds, 5, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen2RoastDuck2Cola = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
+				, activeSeconds_, destroySeconds, 2, 2, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen1Sofa = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
+				, activeSeconds_, destroySeconds, 1, Cfg::unitRadius * 2, 1, numGroups);
+			};
+		auto gen1Sofa2Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 1, 2, Cfg::unitRadius * 3, 1.5, numGroups);
+			};
+		auto gen6Hamburger = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
+				, activeSeconds_, destroySeconds, 6, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen5Cola = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
+				, activeSeconds_, destroySeconds, 5, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen20Chips = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
+				, activeSeconds_, destroySeconds, 20, Cfg::unitRadius * 4, 5, numGroups);
+			};
+		auto gen3RoastDuck2Cola = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
+				, activeSeconds_, destroySeconds, 3, 2, Cfg::unitRadius * 3, 2, numGroups);
+			};
+		auto gen1Laptop = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
+				, activeSeconds_, destroySeconds, 1, Cfg::unitRadius * 2, 1, numGroups);
+			};
+		auto gen1Laptop2RoastDuck = [this](float activeSeconds_, float destroySeconds, int32_t numGroups) {
+			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Laptop, Monster_Roastduck>>()->Init(this
+				, activeSeconds_, destroySeconds, 1, 2, Cfg::unitRadius * 3, 2, numGroups);
+			};
+
 		// fill round monster generator
 		switch (roundId) {
 		case 1:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 18, 3, Cfg::unitRadius * 2, 1, 8);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 5, 20, 2, Cfg::unitRadius * 2, 1, 5);
+			gen3Chips(0, 18, 8);
+			gen2Hamburger(5, 20, 5);
 		}
 		break;
 		case 2:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 18, 3, Cfg::unitRadius * 2, 1, 6);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 6, 20, 2, Cfg::unitRadius * 2, 1, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
-				, 10, 25, 4, 1, Cfg::unitRadius * 3, 2, 3);
+			gen3Chips(0, 18, 6);
+			gen2Hamburger(6, 20, 5);
+			gen4Hamburger1Chips(10, 25, 3);
 		}
 		break;
 		case 3:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 15, 3, Cfg::unitRadius * 2, 1, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 6, 25, 2, Cfg::unitRadius * 2, 1, 6);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group3<Monster_Roastduck, Monster_Cola, Monster_Chips>>()->Init(this
-				, 10, 30, 1, 1, 4,Cfg::unitRadius * 3, 2, 4);
+			gen3Chips(0, 15, 5);
+			gen2Hamburger(6, 25, 6);
+			gen1RoastDuck1Cola4Chips(10, 30, 4);
 		}
 		break;
 		case 4:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 12, 15, Cfg::unitRadius * 2, 1, 2);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Chips>>()->Init(this
-				, 8, 24, 1,3, Cfg::unitRadius * 2, 1.5, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 18, 32, 3,  Cfg::unitRadius * 2, 1.5, 3);
+			gen15Chips(0, 12, 2);
+			gen1RoastDuck3Chips(8,24,4);
+			gen3Cola(18, 32, 3);
 		}
 		break;
 		case 5:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
-				, 0, 15, 4, 1, Cfg::unitRadius * 3, 2, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 6, 20, 3, Cfg::unitRadius * 2, 1, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 10, 25, 3, Cfg::unitRadius * 2, 1.5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
-				, 18, 35, 2, 2, Cfg::unitRadius * 3, 2, 3);
+			gen4Hamburger1Chips(0, 15, 3);
+			gen3Chips(6, 20, 4);
+			gen3Cola(10, 25, 3);
+			gen2RoastDuck2Cola(18, 35, 3);
 		}
 		break;
 		case 6:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 14, 3, Cfg::unitRadius * 2, 1, 6);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
-				, 5, 24, 4, 1, Cfg::unitRadius * 3, 2, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 14, 32, 3, Cfg::unitRadius * 2, 1.5, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
-				, 30, 38, 1, Cfg::unitRadius * 2, 2, 1);
+			gen3Chips(0, 14, 6);
+			gen4Hamburger1Chips(5, 24, 5);
+			gen3Cola(14, 32, 4);
+			gen1Sofa(30, 38, 1);
 		}
 		break;
 		case 7:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 16, 15, Cfg::unitRadius * 2, 4, 2);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Chips>>()->Init(this
-				, 6, 26, 1, 3, Cfg::unitRadius * 2, 1.5, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 18, 35, 1, 2, Cfg::unitRadius * 3, 1.5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
-				, 34, 41, 1, Cfg::unitRadius * 2, 2, 1);
+			gen15Chips(0, 16, 2);
+			gen1RoastDuck3Chips(6, 26, 5);
+			gen1Sofa2Chips(18, 35, 3);
+			gen3Cola(34, 41, 1);
 		}
 		break;
 		case 8:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 0, 16, 6, Cfg::unitRadius * 3, 2, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 6, 25, 3, Cfg::unitRadius * 2, 1.5, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
-				, 18, 35, 3, 2, Cfg::unitRadius * 3, 2, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 36, 44, 1, Cfg::unitRadius * 2, 1, 1);
+			gen6Hamburger(0, 16, 4);
+			gen3Cola(6, 25, 5);
+			gen3RoastDuck2Cola(18, 35, 4);
+			gen1Laptop(36, 44, 1);
 		}
 		break;
 		case 9:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 15, 20, Cfg::unitRadius * 4, 5, 1);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 6, 28, 5, Cfg::unitRadius * 3, 2, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Laptop, Monster_Roastduck>>()->Init(this
-				, 18, 38, 1,2, Cfg::unitRadius * 3, 2, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 40, 47, 1, Cfg::unitRadius * 2, 1, 1);
+			gen20Chips(0, 15, 1);
+			gen5Cola(6, 28, 4);
+			gen1Laptop2RoastDuck(18, 38, 3);
+			gen1Laptop(40, 47, 1);
 		}
 		break;
 		case 10:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
-				, 0, 16, 3, 2, Cfg::unitRadius * 3, 2, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 6, 30, 6, Cfg::unitRadius * 3, 2, 4);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 20, 38, 1, 2, Cfg::unitRadius * 3, 1.5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
-				, 40, 47, 1, Cfg::unitRadius * 2, 2, 1);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 44, 50, 1, Cfg::unitRadius * 2, 1, 1);
+			gen3RoastDuck2Cola(0, 16, 3);
+			gen6Hamburger(6, 30, 4);
+			gen1Sofa2Chips(20, 38, 3);
+			gen1Sofa(40, 47, 1);
+			gen1Laptop(44, 50, 1);
 		}
 		break;
 		case 11:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 16, 20, Cfg::unitRadius * 4, 5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 5, 38, 1, 2, Cfg::unitRadius * 3, 1.5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
-				, 10, 47, 1, Cfg::unitRadius * 2, 2, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 10, 50, 1, Cfg::unitRadius * 2, 1, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 30, 50, 20, Cfg::unitRadius * 4, 5, 2);
+			gen20Chips(0, 16, 3);
+			gen1Sofa2Chips(5, 38, 3);
+			gen1Sofa(10, 47, 3);
+			gen1Laptop(10, 50, 3);
+			gen20Chips(30, 50, 2);
 		}
 		break;
 		case 12:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 0, 16, 20, Cfg::unitRadius * 4, 5, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 3, 38, 1, 2, Cfg::unitRadius * 3, 1.5, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Sofa>>()->Init(this
-				, 5, 47, 1, Cfg::unitRadius * 2, 2, 7);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 5, 60, 1, Cfg::unitRadius * 2, 1, 7);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
-				, 30, 60, 4, 1, Cfg::unitRadius * 3, 2, 5);
-
+			gen20Chips(0, 16, 3);
+			gen1Sofa2Chips(3, 38, 5);
+			gen1Sofa(5, 47, 7);
+			gen1Laptop(5, 60, 7);
+			gen4Hamburger1Chips(30, 60, 5);
 		}
 		break;
 		case 13:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 0, 16, 3, Cfg::unitRadius * 2, 1.5, 7);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 5, 38, 1, 2, Cfg::unitRadius * 3, 1.5, 1);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Roastduck>>()->Init(this
-				, 10, 47, 3, Cfg::unitRadius * 2, 2, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 10, 50, 1, Cfg::unitRadius * 2, 1, 3);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Chips>>()->Init(this
-				, 30, 50, 20, Cfg::unitRadius * 4, 5, 2);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 25, 50, 2, Cfg::unitRadius * 2, 1, 10);
+			gen3Cola(0, 16, 7);
+			gen1Sofa2Chips(5, 38, 1);
+			gen3RoastDuck(10, 47, 3);
+			gen1Laptop(10, 50, 3);
+			gen20Chips(30, 50, 2);
+			gen2Hamburger(25, 50, 10);
 		}
 		break;
 		case 14:
 		{
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Sofa, Monster_Chips>>()->Init(this
-				, 0, 16, 1, 2, Cfg::unitRadius * 3, 1.5, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 3, 38, 3, Cfg::unitRadius * 2, 1.5, 10);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Roastduck, Monster_Cola>>()->Init(this
-				, 15, 47, 3, 2, Cfg::unitRadius * 3, 2, 5);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 5, 60, 1, Cfg::unitRadius * 2, 1, 20);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group2<Monster_Hamburger, Monster_Chips>>()->Init(this
-				, 30, 60, 4, 1, Cfg::unitRadius * 3, 2, 5);
+			gen1Sofa2Chips(0, 16, 5);
+			gen3Cola(3, 38, 10);
+			gen3RoastDuck2Cola(15, 47, 5);
+			gen1Laptop(5, 60, 20);
+			gen4Hamburger1Chips(30, 60, 5);
 		}
 		break;
 		case 15:
 		{
 			// todo house
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Cola>>()->Init(this
-				, 1, 80, 3, Cfg::unitRadius * 2, 1.5, 10);
+			gen3Cola(1, 80, 10);
 			// todo 4 hamburger 1 cola
 			// todo sofa 2 chips
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Laptop>>()->Init(this
-				, 10, 80, 1, Cfg::unitRadius * 2, 1, 15);
-			monsterGenerators.Emplace().Emplace<MonsterGen_Group<Monster_Hamburger>>()->Init(this
-				, 15, 80, 5, Cfg::unitRadius * 3, 2, 8);
+			gen1Laptop(10, 80, 15);
+			gen5Hamburger(15, 80, 8);
 		}
 		break;
 		}
