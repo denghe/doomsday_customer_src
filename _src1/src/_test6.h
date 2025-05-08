41cfbd045ea326f86c483eb2d6552518d24ef07e
denghe <denghe@hotmail.com>
2025-05-08 21:44:42 +0800

    add trail effect ( test6 )

--- _src1/src/_test6.h
+++ _src1/src/_test6.h
@@ -2,10 +2,27 @@
 
 namespace Game {
 
+	template<int32_t len>
+	struct alignas(8) Trail {
+		Stage* stage{};
+		using D = std::pair<XY, XY>;
+		std::array<D, len> data;
+		int32_t cursor{};	// data[cursor] == first node
+		float radius{};
+		D& At(int32_t idx);
+		void StepCursor();
+		void Init(Stage* stage_, XY headPos_, float radius_);
+		void Update(XY headPos_);
+		void Draw();
+	};
+
 	struct Test6 : Stage {
+		xx::Listi32<Trail<64>> trails;
+
 		void MakeUI();
 		void Init() override;
 		void OnWindowSizeChanged() override;
+		void Update() override;
 		void Draw() override;
 	};
 
--- _src1/src/_test6.hpp
+++ _src1/src/_test6.hpp
@@ -3,6 +3,124 @@
 namespace Game {
 
 
+	template<int32_t len>
+	XX_INLINE void Trail<len>::StepCursor() {
+		--cursor;
+		if (cursor < 0) {
+			cursor = len - 1;
+		}
+	}
+
+	template<int32_t len>
+	XX_INLINE Trail<len>::D& Trail<len>::At(int32_t idx) {
+		auto i = cursor + idx;
+		if (i >= len) {
+			i -= len;
+		}
+		return data[i];
+	}
+
+	template<int32_t len>
+	void Trail<len>::Init(Stage* stage_, XY headPos_, float radius_) {
+		assert(!stage);
+		stage = stage_;
+		cursor = 0;
+		radius = radius_;
+		for (int32_t i = 0; i < len; ++i) {
+			data[i].first = headPos_;
+			data[i].second = { 0, radius_ };
+		}
+	}
+
+	template<int32_t len>
+	void Trail<len>::Update(XY headPos_) {
+		auto d = headPos_ - At(0).first;
+		auto r = std::atan2f(d.y, d.x) + float(M_PI / 2);
+		auto cos = std::cosf(r);
+		auto sin = std::sinf(r);
+		StepCursor();
+		auto& head = At(0);
+		head.first = headPos_;
+		head.second = { cos * radius, sin * radius };
+	}
+
+	template<int32_t len>
+	void Trail<len>::Draw() {
+
+		auto texId = gLooper.res._texid_player1;
+		static constexpr auto uvr = gLooper.res._uvrect_player1;
+
+		auto [vi, vs, is] = gLooper.ShaderBegin(gLooper.shaderVertexs).Draw(texId, 4 * (len - 1), 6 * (len - 1));
+
+		static constexpr auto step = uvr.w / (float)(len - 1);
+		for (int32_t i = 1; i < len; ++i) {
+			auto& curr = At(i);
+			auto& last = At(i - 1);
+			auto p1 = curr.first + curr.second;
+			auto p2 = curr.first - curr.second;
+			auto lastP1 = last.first + last.second;
+			auto lastP2 = last.first - last.second;
+
+			auto u1 = uint16_t(uvr.x + step * (len - i - 1));
+			auto u2 = uint16_t(uvr.x + step * (len - i));
+			vs[0] = { .pos = p1, .uv = {u1, uvr.y}, .color = xx::RGBA8_White};
+			vs[1] = { .pos = lastP1, .uv = {u2, uvr.y}, .color = xx::RGBA8_White };
+			vs[2] = { .pos = lastP2, .uv = {u2, uvr.y + uvr.h}, .color = xx::RGBA8_White };
+			vs[3] = { .pos = p2, .uv = {u1, uvr.y + uvr.h}, .color = xx::RGBA8_White };
+
+			is[0] = vi + 0;
+			is[1] = vi + 1;
+			is[2] = vi + 3;
+			is[3] = vi + 1;
+			is[4] = vi + 2;
+			is[5] = vi + 3;
+
+			vi += 4;
+			vs += 4;
+			is += 6;
+		}
+	}
+
+	void Test6::Update() {
+		Stage::Update();
+
+
+		// trail logic
+		for (auto& trail : trails) {
+			trail.Update(gLooper.mouse.pos);
+		}
+	}
+
+
+
+
+	inline void Test6::Init() {
+		ui.Emplace()->Init();
+		MakeUI();
+
+		gridSize = { 60, 60 };
+		mapSize = 128 * gridSize;
+
+		ground.Emplace()->Init(this, mapSize, gLooper.res.ground_cell3);
+		player.Emplace<Player_1>()->Init(this);
+
+		EnvGrass::GenGrass(this, 5);
+
+		camera.scale = Cfg::defaultScale;
+		camera.mapSize = mapSize;
+		camera.newOriginal = camera.original = mapSize * 0.5f;
+
+
+		// trail logic
+		trails.Resize(10000);
+		for (auto& trail : trails) {
+			trail.Init(this, gLooper.mouse.pos, 10);
+		}
+	}
+
+
+
+
 	inline void Test6::MakeUI() {
 		ui->Clear();
 		ui->MakeChildren<xx::Button>()->Init(1, gLooper.pos7 + XY{ 10, -10 }
@@ -20,42 +138,15 @@ namespace Game {
 		MakeUI();
 	}
 
-	inline void Test6::Init() {
-		ui.Emplace()->Init();
-		MakeUI();
 
-		gridSize = { 60, 60 };
-		mapSize = 128 * gridSize;
-
-		ground.Emplace()->Init(this, mapSize, gLooper.res.ground_cell3);
-		player.Emplace<Player_1>()->Init(this);
-
-		EnvGrass::GenGrass(this, 5);
-
-		camera.scale = Cfg::defaultScale;
-		camera.mapSize = mapSize;
-		camera.newOriginal = camera.original = mapSize * 0.5f;
-	}
 
 	inline void Test6::Draw() {
 		Stage::Draw();
 
-		for (int32_t i = 0; i < 500000; ++i) {
-			XY basePos{ rnd.Next<float>(-800, 800), rnd.Next<float>(-400, 400) };
-			auto texId = gLooper.res._texid_player1;
-			static constexpr auto uvr = gLooper.res._uvrect_player1;
-
-			auto [bi, vs, is] = gLooper.ShaderBegin(gLooper.shaderVertexs).Draw(texId, 4, 6);
-			vs[0] = { .pos = basePos + XY{-10, +10}, .uv = {uvr.x, uvr.y}, .color = xx::RGBA8_White };
-			vs[1] = { .pos = basePos + XY{+10, +10}, .uv = {uvr.x + uvr.w, uvr.y}, .color = xx::RGBA8_White };
-			vs[2] = { .pos = basePos + XY{+10, -10}, .uv = {uvr.x + uvr.w, uvr.y + uvr.h}, .color = xx::RGBA8_White };
-			vs[3] = { .pos = basePos + XY{-10, -10}, .uv = {uvr.x, uvr.y + uvr.h}, .color = xx::RGBA8_White };
-			is[0] = bi + 0;
-			is[1] = bi + 1;
-			is[2] = bi + 3;
-			is[3] = bi + 1;
-			is[4] = bi + 2;
-			is[5] = bi + 3;
+		// smooth display for some game content
+		gLooper.res.buff_0->tex->SetGLTexParm<GL_LINEAR>();
+		for (auto& trail : trails) {
+			trail.Draw();
 		}
 	}
 }
