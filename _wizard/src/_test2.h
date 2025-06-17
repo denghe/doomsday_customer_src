ec8b77bb9ae07835626955eaf035b6e40d93150f
denghe <denghe@hotmail.com>
2025-06-17 17:55:18 +0800

    writing...

--- _wizard/src/_test2.h
+++ _wizard/src/_test2.h
@@ -3,24 +3,76 @@
 namespace Game {
 
 	struct Test2;
-	struct M : Drawable {
+	struct alignas(8) Test2Item {
 		Test2* scene{};
-	};
-	struct MB : Drawable {
-		Test2* scene{};
+		xx::XY pos{};
+		virtual int32_t Update() { return 0; };
+		virtual void Draw() {};
+		virtual void DrawLight(float colorPlus_) {};
+		virtual ~Test2Item() {};
 	};
 
-	struct A : M {
-		struct B : MB {
+	/************************************************************/
 
-		};
+	struct M : Test2Item {
+		float radius{}, radians{};
+		float radiansCos{}, radiansSin{};	// cache for radians
+		int32_t stunEndTime{};
+		template<bool force = false> void SetRadians(float radians_);	// for fill cos sin
+	};
+
+	struct MB : Test2Item {
+	};
+
+	/************************************************************/
+
+	struct MB1;
+	struct M1C {
+		XY pos{};
+		float radians{};
+		// todo: more stat? scale?
+	};
+
+	struct M1 : M {
+		static constexpr float cShootDelaySeconds{ 27.f / 120.f };
+		static constexpr int32_t cShootDelayNumFrames{ int32_t(cShootDelaySeconds * Cfg::fps) };
+		std::array<M1C, 4> cannonInfos;
+		int32_t _n{}, _i{}, nextShootTime{};
+		void SyncCannonInfos();
 		void Init(Test2* scene_);
+		int32_t Update() override;
+		void Draw() override;
 	};
-	
+
+	struct MB1 : MB {
+		static constexpr float cRadius{ 40.f };
+		static constexpr float cScaleDurationSeconds{ 27.f / 120.f };
+		static constexpr int32_t cScaleDurationNumFrames{ int32_t(cScaleDurationSeconds * Cfg::fps) };
+		static constexpr float c1_ScaleDurationNumFrames{ 1.f / cScaleDurationNumFrames };
+		static constexpr float cMoveSpeedStep{ 50.f * c1_ScaleDurationNumFrames };
+		xx::Weak<M1> shooter;
+		int32_t _n{};
+		int32_t cannonIndex{}, lifeEndTime{};
+		float radius{}, tarRadius{}, radiusInc{}, radians{};
+		XY posInc{};
+		void Init(xx::Weak<M1> shooter_, int32_t cannonIndex_);
+		int32_t Update() override;
+		void Draw() override;
+	};
+
+	/************************************************************/
+
 	struct Test2 : Scene {
-		xx::Shared<xx::Node> ui;
-		void MakeUI();
-		xx::Shared<M> m;
+
+		Camera camera;
+		xx::Rnd rnd;
+		int32_t time{};
+		float timePool{}, delta{};
+		bool disableLight{};
+		XY mapSize{};
+		XY mousePos{};					// logic pos
+
+		xx::Listi32<xx::Shared<M>> ms;
 		xx::Listi32<xx::Shared<MB>> mbs;
 
 		template<typename T>
@@ -30,6 +82,9 @@ namespace Game {
 		void Update() override;
 		void Draw() override;
 		void OnWindowSizeChanged() override;
+
+		xx::Shared<xx::Node> ui;
+		void MakeUI();
 	};
 
 }
--- _wizard/src/_test2.hpp
+++ _wizard/src/_test2.hpp
@@ -10,31 +10,48 @@ namespace Game {
 			});
 		ui->MakeChildren<xx::Button>()->Init(1, pos7 + XY{ 10, -10 }, anchor7
 			, gLooper.btnCfg, U"A", [&]() {
-				
+				SelectMonster<M1>();
 			});
 	}
 
 	template<typename T>
 	void Test2::SelectMonster() {
 		mbs.Clear();
+		ms.Clear();
 		auto o = xx::MakeShared<T>();
 		o->Init(this);
-		m = std::move(o);
+		ms.Emplace(std::move(o));
 	}
-
 
 	inline void Test2::Init() {
 		UpdateScale();
+		camera.SetBaseScale(scale);
+		camera.SetOriginal({960, 540});
 		MakeUI();
-		SelectMonster<A>();
+		time = 1;
+		SelectMonster<M1>();
 	}
 
 	inline void Test2::Update() {
-
+		camera.Update(rnd, time);
+		for (auto i = mbs.len - 1; i >= 0; --i) {
+			auto& o = mbs[i];
+			if (o->Update()) {
+				mbs.SwapRemoveAt(i);
+			}
+		}
+		for (auto i = ms.len - 1; i >= 0; --i) {
+			auto& o = ms[i];
+			if (o->Update()) {
+				ms.SwapRemoveAt(i);
+			}
+		}
+		++time;
 	}
 
 	inline void Test2::Draw() {
-
+		for (auto& o : mbs) o->Draw();
+		for (auto& o : ms) o->Draw();
 
 		gLooper.DrawNode(ui);
 	}
@@ -43,8 +60,133 @@ namespace Game {
 		MakeUI();
 	}
 
+	/********************************************************************************/
 
-	inline void A::Init(Test2* scene_) {
-		scene = scene_;
+	template<bool force>
+	XX_INLINE void M::SetRadians(float radians_) {
+		if constexpr (!force) {
+			if (radians == radians_) return;
+		}
+		radians = radians_;
+		radiansCos = std::cosf(radians_);
+		radiansSin = std::sinf(radians_);
 	}
+
+	/********************************************************************************/
+
+	XX_INLINE void M1::SyncCannonInfos() {
+
+		// pos + XY{ cShootOffset.x * radiansCos - cShootOffset.y * radiansSin
+		//	, cShootOffset.x * radiansSin + cShootOffset.y * radiansCos };
+
+		// 1, 0
+		cannonInfos[0].pos = pos + XY{ radius * radiansCos, radius * radiansSin };
+		cannonInfos[0].radians = radians + 0.f;
+		// 0, 1
+		cannonInfos[1].pos = pos + XY{ -radius * radiansSin, radius * radiansCos };
+		cannonInfos[1].radians = radians + float(M_PI) * 0.5f;
+		// -1, 0
+		cannonInfos[2].pos = pos + XY{ -radius * radiansCos, -radius * radiansSin };
+		cannonInfos[2].radians = radians + float(M_PI);
+		// 0, -1
+		cannonInfos[3].pos = pos + XY{ radius * radiansSin, -radius * radiansCos };
+		cannonInfos[3].radians = radians + float(M_PI) * 1.5f;
+	}
+
+	inline void M1::Init(Test2* scene_) {
+		scene = scene_;
+		pos = {960, 540};
+		radius = 100.f;
+		SetRadians<true>(0.f);
+		SyncCannonInfos();
+	}
+
+	inline int32_t M1::Update() {
+		XX_BEGIN(_n);
+		// todo
+		//scene->mbs.Emplace().Emplace<MB1>()->Init(xx::WeakFromThis(this), 0);
+		XX_YIELD_I(_n);
+		XX_END(_n);
+		return 0;
+	}
+
+	inline void M1::Draw() {
+		// 4 cannon + 1 body
+		// todo
+		auto baseScale = radius * 2.f / gLooper.res._size_ui_gear.x * scene->camera.scale;
+		auto q = gLooper.ShaderBegin(gLooper.shaderQuadInstance)
+			.Draw(gLooper.res._texid_ui_gear, 5);
+		// cannons
+		for (int32_t i = 0; i < 4; ++i) {
+			auto& f = *gLooper.res.ui_run3;
+			q[i].pos = scene->camera.ToGLPos(cannonInfos[i].pos);
+			q[i].anchor = { 0, 0.5f };
+			q[i].scale = baseScale * 0.1f;
+			q[i].radians = cannonInfos[i].radians;
+			q[i].colorplus = stunEndTime >= scene->time ? 100000.f : 1.f;
+			q[i].color = xx::RGBA8_White;
+			q[i].texRect.data = f.textureRect.data;
+		}
+		// body
+		auto& f = *gLooper.res.ui_gear;
+		q[4].pos = scene->camera.ToGLPos(pos);
+		q[4].anchor = 0.5f;
+		q[4].scale = baseScale;
+		q[4].radians = radians;
+		q[4].colorplus = stunEndTime >= scene->time ? 100000.f : 1.f;
+		q[4].color = xx::RGBA8_White;
+		q[4].texRect.data = f.textureRect.data;
+	}
+
+	/********************************************************************************/
+
+	inline void MB1::Init(xx::Weak<M1> shooter_, int32_t cannonIndex_) {
+		scene = shooter_->scene;
+		auto& ci = shooter_->cannonInfos[cannonIndex_];
+		shooter = std::move(shooter_);
+		cannonIndex = cannonIndex_;
+		pos = ci.pos;
+		radius = 0.f;
+		tarRadius = cRadius;
+		radiusInc = (tarRadius - radius) * c1_ScaleDurationNumFrames;
+		radians = ci.radians;
+	}
+
+	inline int32_t MB1::Update() {
+		// todo: hit player check
+		XX_BEGIN(_n);
+		while (radius < tarRadius) {
+			if (!shooter) {
+				// todo: death effect
+				return 1;
+			}
+			radius += radiusInc;
+			pos = shooter->cannonInfos[cannonIndex].pos;
+			radians = shooter->cannonInfos[cannonIndex].radians;
+			XX_YIELD_I(_n);
+		}
+		radius = tarRadius;
+		lifeEndTime = scene->time + int32_t(Cfg::fps * 10.f);
+		posInc = { std::cosf(radians) * cMoveSpeedStep, std::sinf(radians) * cMoveSpeedStep };
+		while (lifeEndTime >= scene->time) {
+			pos += posInc;
+			XX_YIELD_I(_n);
+		};
+		XX_END(_n);
+		return 0;
+	}
+
+	inline void MB1::Draw() {
+		auto& f = *gLooper.res.ui_circle;
+		auto q = gLooper.ShaderBegin(gLooper.shaderQuadInstance)
+			.Draw(f.tex, 1);
+		q->pos = scene->camera.ToGLPos(pos);
+		q->anchor = *f.anchor;
+		q->scale = radius * 2.f / f.spriteSize.x * scene->camera.scale;
+		q->radians = radians;
+		q->colorplus = 1.f;
+		q->color = xx::RGBA8_White;
+		q->texRect.data = f.textureRect.data;
+	}
+
 }
